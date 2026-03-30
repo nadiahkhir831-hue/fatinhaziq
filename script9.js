80253899358ede9424d7dfccba69e5eb605e5e77
nadiahkhir831-hue <nadiahkhir831@gmail.com>
2026-03-30 10:21:20 +0800

    Update script9.js

--- script9.js
+++ script9.js
@@ -6,19 +6,20 @@ function updateCart(){
     rows.forEach(row => {
         let qty = row.querySelector(".qty").value;
         let price = row.querySelector(".price").textContent;
-        let amount = qty * price;
 
-        row.querySelector(".amount").textContent = amount;
+        let amount = qty * price;
+        row.querySelector(".amount").textContent = "RM" + amount.toFixed(2);
+
         subtotal += amount;
     });
 
-    let tax = subtotal * 0.06;
-    let shipping = 50;
+    let tax = subtotal * 0.05;
+    let shipping = 40;
     let grand = subtotal + tax + shipping;
 
-    document.getElementById("subtotal").textContent = subtotal.toFixed(2);
-    document.getElementById("tax").textContent = tax.toFixed(2);
-    document.getElementById("grandtotal").textContent = grand.toFixed(2);
+    document.getElementById("subtotal").textContent = "RM" + subtotal.toFixed(2);
+    document.getElementById("tax").textContent = "RM" + tax.toFixed(2);
+    document.getElementById("grandtotal").textContent = "RM" + grand.toFixed(2);
 }
 
 document.querySelectorAll(".qty").forEach(input => {
