dc23cbac627c6072a9979414a44ba5063c7746fe
mohdhaziq-5599 <mohdhaziq5599@gmail.com>
2026-03-30 12:03:36 +0800

    Update travel.js

--- travel.js
+++ travel.js
@@ -1,32 +1,39 @@
-window.addEventListener("load", function() {  
-    var thumbs = document.getElementById("thumbnails"); 
-    // use event delegation ... instead of having a handler for 
-    // each thumbnail, put the handler on the parent container instead 
-thumbs.addEventListener("click", function (e) { 
-        // user could click on different things in the parent container, 
-        // so we need to check if user has clicked on a thumbnail image 
-        if (e.target.nodeName.toLowerCase() == 'img') { 
-            // get image filename of clicked thumbnail 
-            var clickedImageSource = e.target.src;    
-            // replace the folder name of the image  
-            var newSrc = clickedImageSource.replace("small", "medium");             
-            // change the filename and title attribute of the large image  
-            // to the new one (the one just clicked on) 
-            var featuredImage = document.querySelector("#featured img"); 
-featuredImage.src = newSrc;  
-featuredImage.title = e.target.title; 
- }})}); 
-// add mouse over/out handlers for large image 
-    var featured = document.getElementById("featured"); 
-featured.addEventListener("mouseover", function (e) { 
-   // fade the caption into view 
-  var caption = document.querySelector("#featured figcaption"); 
-caption.style.transition = "opacity 1.5s"; 
-caption.style.opacity = 0.80;         
-caption.innerHTML = document.querySelector("#featured img").title; 
-    }); 
-featured.addEventListener("mouseout", function (e) {    
-        // fade the caption into invisibility 
-        var caption = document.querySelector("#featured figcaption"); 
-caption.style.transition = "opacity 1.5s"; 
-caption.style.opacity = 0; });
+window.addEventListener("load", function() {
+    // Get the container for the thumbnails
+    var thumbs = document.getElementById("thumbnails");
+
+    // Use event delegation: handle clicks on thumbnails via parent container
+    thumbs.addEventListener("click", function(e) {
+        // Check if the clicked element is an image
+        if (e.target.nodeName.toLowerCase() === 'img') {
+            // Get the image source of the clicked thumbnail
+            var clickedImageSource = e.target.src;
+
+            // Replace "small" with "medium" in the image path
+            var newSrc = clickedImageSource.replace("small", "medium");
+
+            // Update the featured image
+            var featuredImage = document.querySelector("#featured img");
+            featuredImage.src = newSrc;
+            featuredImage.title = e.target.title;
+        }
+    });
+
+    // Get the featured image container
+    var featured = document.getElementById("featured");
+
+    // Show caption on mouseover
+    featured.addEventListener("mouseover", function() {
+        var caption = document.querySelector("#featured figcaption");
+        caption.style.transition = "opacity 1.5s";
+        caption.style.opacity = 0.8;
+        caption.innerHTML = document.querySelector("#featured img").title;
+    });
+
+    // Hide caption on mouseout
+    featured.addEventListener("mouseout", function() {
+        var caption = document.querySelector("#featured figcaption");
+        caption.style.transition = "opacity 1.5s";
+        caption.style.opacity = 0;
+    });
+});
